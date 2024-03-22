c83421db984d490c884aa531445ff08318c1fbed
Sebastian W <sebwilczega@gmail.com>
2024-03-22 19:06:33 +0000

    initialise rows and columns

--- bingo.js
+++ bingo.js
@@ -1,3 +1,10 @@
-function bingo({
+function bingo(numbers, board) {
+    const rows = board.map(row => row.split('').map(Number));
+    const columns = [[],[],[],[],[]];
 
-})
+    // fill columns
+    rows.forEach(row => {
+        row.forEach(columns[])
+    });
+    // iterate arrays and columns to look for bingo
+};
