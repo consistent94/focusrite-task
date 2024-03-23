203891287b0508c4a9b258d1fb5b70d4d52cbb92
Sebastian W <sebwilczega@gmail.com>
2024-03-23 13:57:31 +0000

    fetch data from txt file

--- bingo.js
+++ bingo.js
@@ -1,7 +1,18 @@
+import fs from 'fs';
+
+const data = fs.readFileSync('data.txt', { encoding: 'utf8' });
+const sections = data.split('---\n').map(section => section.trim());
+const numbers = sections[0].split(',').map(Number);
+const boards = sections.slice(1).map(section => 
+    section.split('\n')
+);
+
 // I against squid PART 1
 
 function bingo(numbers, board) {
-    const rows = board.map(row => row.split(' ').map(Number));
+    const rows = board.map(row => 
+        row.split(' ').map(Number)
+    ); 
     const columns = [[], [], [], [], []];  
 
     // fill columns
@@ -15,64 +26,29 @@ function bingo(numbers, board) {
     for (let i = 0; i < 5; i++) {
         // every method
         if (rows[i].every(num => numbers.includes(num))) {
-            return true; // rows bingo, squid lost
+            return ('Bingo! This card beats squid!'); // rows bingo, squid lose
         }
 
         if (columns[i].every(num => numbers.includes(num))) {
-            return true; // columns bingo, I win
+            return ('Bingo! Columns guranteed you moving pass squid'); // columns bingo, I win
         }
     }
 
-    return false; // squid wins
+    return ('No bingo. Squid was too strong'); // squid wins
 };
 
-// tessssst
-const numbers = [7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1];
-const board = [
-    '22 13 17 11 0',
-    '8 2 23 4 24',
-    '21 9 14 16 7',
-    '6 10 3 18 5',
-    '1 12 20 15 19'
-];
-
-console.log(bingo(numbers, board));
+console.log(bingo(numbers, boards[0]));
 
 // I against squid PART 2
 
 function guaranteedWinBoard(numbers, boards) {
     for (let i = 0; i < boards.length; i++) {
         if (bingo(numbers, boards[i])) {
-            return ('winning board guaranteed');
+            return ('To continue my journey passed squid I could pick a board at index ' + (i + 1));
         };
     };
 
-    return ('no guaranteed win');
+    return ('No luck, squid happened to be stronger this time.');
 }
 
-const numbers2 = [7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1];
-const boards = [
-    [
-        '22 13 17 11 0',
-        '8 2 23 4 24',
-        '21 9 14 16 7',
-        '6 10 3 18 5',
-        '1 12 20 15 19'
-    ],
-    [
-        '3 15 0 2 22',
-        '9 18 13 17 5',
-        '19 8 7 25 23',
-        '20 11 10 24 4',
-        '14 21 16 12 6'
-    ],
-    [   
-        '14 21 17 24  4',
-        '10 16 15  9 19',
-        '18  8 23 26 20',
-        '22 11 13  6  5',
-        '2 0 12 3 7'
-    ]
-];
-
-console.log(guaranteedWinBoard(numbers2, boards))
+console.log(guaranteedWinBoard(numbers, boards))
