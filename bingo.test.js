882ae81b57eb1f0ae41d1c54c963f93538382a89
Sebastian W <sebwilczega@gmail.com>
2024-03-23 14:21:44 +0000

    tests

---
+++ bingo.test.js
@@ -0,0 +1,14 @@
+import { bing, guaranteedWinBoard } from './bingo.js';
+
+describe('Bingo functionality', () => {
+    test('Bingo identifies winning card correctly', => {
+        const numbers = [5, 25, 53, 3, 7, 4, 77, 74, 35, 20, 32];
+        const boards = [
+            '22 13 17 11 0',
+            '8 2 23 4 24',
+            '21 9 14 16 7',
+            '6 10 3 18 5',
+            '1 12 20 15 19'
+      ];
+    })
+})
